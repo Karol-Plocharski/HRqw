67f99dfb7b3792335ba403d602177ba41fdff8e0
Maciej Kopciał <maciej.kopcial@gmail.com>
2019-11-17 11:43:57 +0100

    Dodane EmployeeDto.java oraz DepartmentDto.java,; Daty będ wpisywane tak: Date date = new SimpleDateFormat("dd/MM/yyyy").parse("12/05/2015");

--- src/dto/EmployeeDto.java
+++ src/dto/EmployeeDto.java
@@ -1,4 +1,54 @@
 package dto;
 
+import java.util.Date;
+
 public class EmployeeDto {
+    private String name;
+    private String surname;
+    private String occupation;
+    private Date date;
+    public DepartmentDto getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(DepartmentDto department) {
+        this.department = department;
+    }
+
+    private DepartmentDto department;
+
+    private int numberOfExperience;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public Date getDane() {
+        return date;
+    }
+
+    public void setDane(Date dane) {
+        this.date = dane;
+    }
+
 }
