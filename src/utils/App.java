99b5e19090386b330d2a8ec4c56821e86ddf1f4e
KamilaoPL <stepniewski.kamil@gmail.com>
2019-11-17 15:48:00 +0100

    Next Code

--- src/utils/App.java
+++ src/utils/App.java
@@ -10,27 +10,46 @@ import java.util.Scanner;
 
 public class App {
     public void run() {
+
         Scanner input = new Scanner(System.in);
         System.out.println("|| *Human Resources Application* ||");
+
         boolean isValueOK = false;
+
         while (!isValueOK) {
-            System.out.println("1. Create an employee");
-            System.out.println("2. Search for an employee");
-            String value = input.nextLine();
-            //zmiana stringa w inta
-            if (Integer.parseInt(value) == 1) {
-                //Ustawiamy Dane
-                //List<EmployeeDto> employee = new EmployeeDao().setData();
-                //Wyswietlamy dane
-                //new EmployeeDao().displayData(employee);
-                isValueOK = true;
-            } else if (Integer.parseInt(value) == 2) {
-                //List<DepartmentDto> department = new DepartmentDao().setData();
-                //new DepartmentDao().displayData(department);
-                isValueOK = true;
-            } else {
-                System.out.println("Enter a number between 1-2");
+
+            try {
+
+                System.out.println("1. Create an employee");
+                System.out.println("2. Search for an employee");
+                String value = input.nextLine();
+
+                if (Integer.parseInt(value) == 1) {
+                    boolean isEmployeeOK = false;
+                    while (!isEmployeeOK) {
+                        //Ustawiamy Dane
+                        // List<EmployeeDto> employee = new EmployeeDao().setData();
+                        System.out.println("Do you want to add another employee? (y/n)");
+                        String answer = input.nextLine();
+                        if (answer.equals("n")) {
+                            isEmployeeOK = true;
+                        }
+                    }
+                } else if (Integer.parseInt(value) == 2) {
+                    //List<DepartmentDto> department = new DepartmentDao().setData();
+                    //new DepartmentDao().displayData(department);
+
+
+                } else {
+                    System.out.println("Enter a number between 1-2");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Do you think you are funny? Enter only a number between 1-2");
             }
+            System.out.println("Do you want to terminate the application? (y/n)");
+            input.nextLine();
         }
     }
 }
+
