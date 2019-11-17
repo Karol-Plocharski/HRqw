723e683a6ef41f25ca9eecdd3fb0a818e738c5ac
Karol-Plocharski <karol_plocharski@o2.pl>
2019-11-17 12:40:56 +0100

    my commit

--- src/dao/DepartmentDao.java
+++ src/dao/DepartmentDao.java
@@ -1,4 +1,35 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DepartmentDao {
+    public DepartmentDao (String shortname, String fullname) {
+    }
+
+    public List<DepartmentDao> setData() {
+        String decision = "T";
+        List<DepartmentDao> department = new ArrayList<DepartmentDao>();
+        Scanner input = new Scanner(System.in);
+        while (decision.equals("T")) {
+
+            System.out.println("HR");
+            System.out.println("IT");
+            System.out.println("FN");
+            String shortname = input.nextLine();
+            System.out.println("Human Resourcec");
+            System.out.println("Information Technology");
+            System.out.println("Finance");
+            String fullname = input.nextLine();
+
+            department.add(new DepartmentDao(shortname, fullname));
+
+            System.out.println("Podaj kolejnego pracownika");
+            decision = input.nextLine();
+        }
+        return department ;
+
+    }
+}
 }
--- src/dao/EmployeeDao.java
+++ src/dao/EmployeeDao.java
@@ -1,4 +1,43 @@
 package dao;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeeDao {
+    public class employee {
+
+        public List<EmployeeDao> setData() {
+            String decision = "T";
+            List<EmployeeDao> employees = new ArrayList<EmployeeDao>();
+            Scanner input = new Scanner(System.in);
+            while (decision.equals("T")) {
+
+                System.out.println("Podaj imię");
+                String name = input.nextLine();
+
+                System.out.println("Podaj nazwisko");
+                String surname = input.nextLine();
+
+                System.out.println("Poziom zaawansowania");
+                String occupation = input.nextLine();
+                //dodajemy nowego studenta do listy na podstawie wpisanych danych
+                employees.add(new EmployeeDao(name, surname, occupation));
+
+                System.out.println("Czy chcesz podawać dalej T/N");
+                decision = input.nextLine();
+            }
+            return employees;
+        }
+
+        public void displayData(List<StudentDto> students) {
+            for (StudentDto student:students){
+                //pobieramy index studenta dla listy students
+                System.out.print("|"+(students.indexOf(student)+1)+"|");
+                System.out.print("|"+student.getName()+"|");
+                System.out.print("|"+student.getSurname()+"|");
+                System.out.print("|"+student.getClassNo()+"|");
+                System.out.println();
+            }
+        }
+    }
 }
